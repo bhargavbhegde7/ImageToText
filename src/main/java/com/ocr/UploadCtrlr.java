71dcab80911a0d3c235ac29c7509cc035a6474b9
bhargav <bhargavbhegde7@gmail.com>
2016-12-16 21:26:43 +0530

    gradle build script

--- src/main/java/com/ocr/UploadCtrlr.java
+++ src/main/java/com/ocr/UploadCtrlr.java
@@ -1,10 +1,15 @@
 package com.ocr;
 
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.lept;
+import org.bytedeco.javacpp.tesseract;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import static org.bytedeco.javacpp.lept.pixDestroy;
+import static org.bytedeco.javacpp.lept.pixRead;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -14,13 +19,34 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 public class UploadCtrlr {
 
-    private String getTextFromImage(MultipartFile imageFile){
+    private String getTextFromImage1(MultipartFile imageFile){
         return "dummy text";
+    }
+
+    public String getTextFromImage(String fileName){
+        BytePointer outText;
+
+        tesseract.TessBaseAPI api = new tesseract.TessBaseAPI();
+        if (api.Init(".", "ENG") != 0) {
+            return "Couldn't initialize tesseract";
+        }
+
+        lept.PIX image = pixRead(fileName);
+
+        api.SetImage(image);
+        outText = api.GetUTF8Text();
+        String string = outText.getString();
+
+        api.End();
+        outText.deallocate();
+        pixDestroy(image);
+
+        return string;
     }
 
     @RequestMapping(method = POST)
     public String postImage(@RequestParam("inputImage") MultipartFile imageFile){
-        return getTextFromImage(imageFile)+ " --- " +imageFile.getOriginalFilename();
+        return getTextFromImage1(imageFile)+ " --- " +imageFile.getOriginalFilename();
     }
 
     @RequestMapping(method = GET)
