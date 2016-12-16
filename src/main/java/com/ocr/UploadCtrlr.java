fc8553e168c0e14455070df2edda9f37ec5c6f24
bhargav <bhargavbhegde7@gmail.com>
2016-12-16 22:56:31 +0530

    opencv dependency added

--- src/main/java/com/ocr/UploadCtrlr.java
+++ src/main/java/com/ocr/UploadCtrlr.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 import static org.bytedeco.javacpp.lept.pixDestroy;
 import static org.bytedeco.javacpp.lept.pixRead;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -21,6 +26,38 @@ public class UploadCtrlr {
 
     private String getTextFromImage1(MultipartFile imageFile){
         return "dummy text";
+    }
+
+    private void convert(){
+        BufferedImage image;
+        int width;
+        int height;
+        try {
+            File input = new File("C:\\Users\\goodbytes\\Desktop\\Capture.jpg");
+            image = ImageIO.read(input);
+            width = image.getWidth();
+            height = image.getHeight();
+
+            for(int i=0; i<height; i++){
+
+                for(int j=0; j<width; j++){
+
+                    Color c = new Color(image.getRGB(j, i));
+                    int red = (int)(c.getRed() * 0.299);
+                    int green = (int)(c.getGreen() * 0.587);
+                    int blue = (int)(c.getBlue() *0.114);
+                    Color newColor = new Color(red+green+blue,
+
+                            red+green+blue,red+green+blue);
+
+                    image.setRGB(j,i,newColor.getRGB());
+                }
+            }
+
+            File ouptut = new File("grayscale.jpg");
+            ImageIO.write(image, "jpg", ouptut);
+
+        } catch (Exception e) {}
     }
 
     public String getTextFromImage(String fileName){
@@ -47,6 +84,7 @@ public class UploadCtrlr {
     @RequestMapping(method = POST)
     public String postImage(@RequestParam("inputImage") MultipartFile imageFile){
         //return getTextFromImage1(imageFile)+ " --- " +imageFile.getOriginalFilename();
+        convert();
         return getTextFromImage("")+ " --- " +imageFile.getOriginalFilename();
     }
 
